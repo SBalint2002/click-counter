59c9d0568f525e3dd11d2abf76a37c6717694a73
Sárándi <sarandi.balint@gmail.com>
2022-09-23 08:53:30 +0200

    3.a

--- app/src/main/java/hu/petrik/clickcounter/MainActivity.java
+++ app/src/main/java/hu/petrik/clickcounter/MainActivity.java
@@ -3,27 +3,51 @@ package hu.petrik.clickcounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button plusz;
-    private TextView szam;
+    private TextView szamlalo;
     private Button minusz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+        plusz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int szam = Integer.parseInt(szamlalo.getText().toString()) + 1;
+                szamlalo.setText(String.valueOf(szam));
+            }
+        });
+
+        minusz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int szam = Integer.parseInt(szamlalo.getText().toString()) - 1;
+                szamlalo.setText(String.valueOf(szam));
+            }
+        });
+
+        szamlalo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                szamlalo.setText("0");
+            }
+        });
     }
 
 
     private void init(){
-        //layout = findViewById(R.id.layout);
         plusz = findViewById(R.id.plusz);
-        szam = findViewById(R.id.szam);
+        szamlalo = findViewById(R.id.szamlalo);
         minusz = findViewById(R.id.minusz);
-        //rnd = new Random();
     }
 }
